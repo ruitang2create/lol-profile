a06f56adf336274ff8a18d4597c2c9a501c871cd
ruitang2create <trtdytr93629@gmail.com>
2021-06-17 03:39:59 -0500

    Displayed summoner spells and items for each match record

--- components/MatchRecord.js
+++ components/MatchRecord.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import styles from '../styles/MatchHistory.module.css';
 import championsBgs from '../static/data/championsBackground.json';
-import championsDB from '../public/official_assets/11.12.1/data/en_US/champion.json';
 import runesDB from '../static/data/runes.json';
+import summonerspellsDB from '../static/data/summonerspells.json';
 
 const MatchRecord = ({ summonerName, playerInfo, matchDetails }) => {
     const playedChamp = championsBgs[playerInfo.champion].name;
@@ -20,21 +20,29 @@ const MatchRecord = ({ summonerName, playerInfo, matchDetails }) => {
 
     const playerParticipantId = identifyPlayer(summonerName, matchDetails.participantIdentities);
     const playerDTO = matchDetails.participants[playerParticipantId];
-    console.log(playerDTO);
+    const playerItems = [playerDTO.stats.item0, playerDTO.stats.item1, playerDTO.stats.item2, playerDTO.stats.item3, playerDTO.stats.item4, playerDTO.stats.item5];
+    console.log(playerDTO)
 
     const getRuneIconPath = (runesId) => {
         return `/official_assets/img/${runesDB[runesId].icon}`
     }
 
+    const getSummonerSpellIconPath = (spellId) => {
+        const spellPath = summonerspellsDB[spellId].image.full;
+        return `/official_assets/11.12.1/img/spell/${spellPath}`;
+    }
+
     return (
         <div className={styles.MatchRecord}>
             <div className={styles.col1}>
-                <Image
-                    className={styles.ChampionProfile}
-                    src={`/official_assets/11.12.1/img/champion/${playedChamp}.png`}
-                    width={200}
-                    height={200}
-                />
+                <div className={styles.champProfileContainer}>
+                    <Image
+                        className={styles.ChampionProfile}
+                        src={`/official_assets/11.12.1/img/champion/${playedChamp}.png`}
+                        width={200}
+                        height={200}
+                    />
+                </div>
                 <div className={styles.Runes}>
                     <div className={styles.MainRune}>
                         <Image
@@ -55,8 +63,38 @@ const MatchRecord = ({ summonerName, playerInfo, matchDetails }) => {
                 </div>
             </div>
             <div className={styles.col2}>
-                <div className={styles.SummonerSpells}></div>
-                <div className={styles.Items}></div>
+                <div className={styles.SummonerSpellsContainer}>
+                    <div className={styles.SummonerSpells}>
+                        <Image
+                            className={styles.SpellImg}
+                            src={getSummonerSpellIconPath(playerDTO.spell1Id)}
+                            width={40}
+                            height={40}
+                        />
+                    </div>
+                    <div className={styles.SummonerSpells}>
+                        <Image
+                            className={styles.SpellImg}
+                            src={getSummonerSpellIconPath(playerDTO.spell2Id)}
+                            width={40}
+                            height={40}
+                        />
+                    </div>
+                </div>
+                <div className={styles.Items}>
+                    {playerItems.map((item, index) => {
+                        return (
+                            <div key={index} className={styles.ItemContainer}>
+                                <Image
+                                    className={styles.ItemImg}
+                                    src={`/official_assets/11.12.1/img/item/${item}.png`}
+                                    width={30}
+                                    height={30}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
             <div className={styles.col3}>
                 <div className={styles.KdaIndex}></div>
